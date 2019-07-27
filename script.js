27f3ef8db470f1c2bc8e0478286477dc626f0ba9
Brandon Wiedemeier <brandon.wiedemeier@gmail.com>
2019-07-26 19:00:51 -0500

    input fields adding correctly

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 console.log('javascript is linked');
 console.log('dom is loading');
-
+// DOM set up
 $(document).ready(readyNow);
 
+// create function readyNow to collect all functions
 function readyNow() {
     console.log('DOM is Ready!');
     // event listeners
     $('#submitButton').on('click', submitButton)
 }
-
+// submit button - add employee data to the table
 function submitButton() {
     let firstName = $('#firstNameIn').val();
     let lastName = $('#lastNameIn').val();
@@ -16,19 +17,25 @@ function submitButton() {
     let titleName = $('#titleNameIn').val();
     let annualName = $('#annualNameIn').val();
 
-    $('#fistNameDom').append(firstName);
-    $('#lastNameDom').append(lastName);
-    $('#idNumberDom').append(idNumber);
-    $('#titleNameDom').append(titleName);
-    $('#annualNameDom').append(annualName);
+    $('#totalTable').append(`<tr><td>${firstName}</td>
+                                <td>${lastName}</td>
+                                <td>${idNumber}</td>
+                                <td>${titleName}</td>
+                                <td>${annualName}</td>
+                                </tr>`);
+    
+    // $('#firstNameDom').empty('');                            
+    // $('#totalTable').append(firstName);
+    // $('#lastNameDom').append(lastName);
+    // $('#idNumberDom').append(idNumber);
+    // $('#titleNameDom').append(titleName);
+    // $('#annualNameDom').append(annualName);
+    
         
 }
-// DOM set up
-// ready function
-
-// submit button - add employee data to the table
 
 // calculate total monthly and update on the dom
+
 
 // clear input fields
 
