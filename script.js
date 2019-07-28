dc00fb18850cf4027dd469375e718ee11607299e
Brandon Wiedemeier <brandon.wiedemeier@gmail.com>
2019-07-28 15:57:48 -0500

    delete button working properly

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ function readyNow() {
     // event listeners
     $('#submitButton').on('click', submitButton)
     $('#submitButton').on('click', calMonthlyTotals)
+    $('#totalTable').on('click', '.deleteButton', clickDeleteButton)
 }
 // submit button - add employee data to the table
 function submitButton() {
@@ -26,48 +27,35 @@ function submitButton() {
                                 <td>${idNumber}</td>
                                 <td>${titleName}</td>
                                 <td>${annualName}</td>
-                                <td><button>Delete</button></td>
+                                <td><button class="deleteButton">Delete</button></td>
                                 </tr>`);
 
     totalAnnualSalary += parseFloat(annualName);
-    
-    // ($('#annualNameIn').val());
-    
+
     $('#firstNameIn').val(''); 
     $('#lastNameIn').val('');
     $('#idNumberIn').val('');
     $('#titleNameIn').val('');
     $('#annualNameIn').val('');
 
-    // calMonthlyTotals()
-
 }
 // let totalMonthly = 0; 
 // // // calculate total monthly and update on the dom
 function calMonthlyTotals() {
-
-    // totalAnnualSalary += parseFloat(($('#annualNameIn').val()));
     console.log(totalAnnualSalary);
 
     totalMonthlySalary = (totalAnnualSalary / 12 );
     console.log(totalMonthlySalary);
     
-
     $('#totalMonthly').text('Total Monthly: $' + totalMonthlySalary );
     console.log('working');
     return totalMonthlySalary
-
-   
-    
-// totalMonthlySalary = {
-//     Total: $('annualNameIn').val()
-
+}
+// delete button - delete this row from the table
+function clickDeleteButton() {
+    // $('#deleteButton').on('click', clickDeleteButton)
+    console.log('this is working');
+    $(this).parent().parent().remove()  
 }
 
- 
-
-
-// clear input fields
-
-// delete button - delete this row from the table
 console.log('dom has loaded');
